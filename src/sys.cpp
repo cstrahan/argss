09c26e2bb2912dcfa818d3727917fd849c622eff
vgvgf <vgvgf@a70b67f4-0116-4799-9eb2-a6e6dfe7dbda>
2009-11-12 22:22:06 +0000

    Fixed error when reading .ini files.
    git-svn-id: https://argss.svn.sourceforge.net/svnroot/argss/trunk@5 a70b67f4-0116-4799-9eb2-a6e6dfe7dbda

--- src/sys.cpp
+++ src/sys.cpp
@@ -10,18 +10,19 @@ void Init_Sys()
     SI_Error rc = ini.LoadFile(".\\ARGSS.ini");
     if (rc < 0) {
         fprintf(stderr, "ARGSS could not open \"ARGSS.ini\" file.\n");
+		exit(-1);
     }
     Sys_ScriptsPath = ini.GetValue("Game", "Scripts", "NULL");
     if (Sys_ScriptsPath == "NULL") {
         fprintf(stderr, "ARGSS could not find the scripts path on \"ARGSS.ini\".\n");
+		exit(-1);
     }
     Sys_Title = ini.GetValue("Game", "Title", "ARGSS");
-    int i, e;
+    int i;
     char rtp[4];
     for(i = 0; i < 3; i++) {
-        e = i;
-        sprintf(rtp, "RTP%d", ++e);
-        Sys_RTPS[e] = ini.GetValue("Game", rtp, "");
+        sprintf(rtp, "RTP%d", i + 1);
+        Sys_RTPS[i] = ini.GetValue("Game", rtp, "");
     }
     Sys_Res[0] = ini.GetLongValue("Game", "ResW", 640);
     Sys_Res[1] = ini.GetLongValue("Game", "ResH", 480);
